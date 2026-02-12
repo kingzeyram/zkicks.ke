cb9a940ab15c8fba2a5d35ea14bb6d8c7e266469
ramsey <lungahoramsey2005@gmail.com>
2026-02-12 11:25:46 +0300

    update

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,7 +4,7 @@ import { stripe } from "@/lib/stripe";
 export default async function ProductsPage() {
   const products = await stripe.products.list({
     expand: ["data.default_price"],
-    limit: undefined,
+    limit: 1000,
   });
   return (
     <div className="pb-8">
