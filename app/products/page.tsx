c2a39f0a19b95e0399df25f516373ead480d33a3
ramsey <lungahoramsey2005@gmail.com>
2026-02-12 10:41:29 +0300

    update

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,14 +2,14 @@ import { ProductList } from "@/components/ui/product-list"
 import { stripe } from "@/lib/stripe";
 
 export default async function ProductsPage() {
-     const products = await stripe.products.list({
-        expand:["data.default_price"],
-        limit:5,
-      });
-    return (
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: undefined,
+  });
+  return (
     <div className="pb-8">
-        <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">All our products</h1>
-        <ProductList products={products.data} />
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">All our products</h1>
+      <ProductList products={products.data} />
     </div>
-    );
+  );
 }
