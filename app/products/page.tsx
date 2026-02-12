2efe7c484b3080c435e8fcf52c5563418cbac85d
ramsey <lungahoramsey2005@gmail.com>
2026-02-12 11:17:15 +0300

    update

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -8,7 +8,7 @@ export default async function ProductsPage() {
   });
   return (
     <div className="pb-8">
-      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">All our products</h1>
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">All our products.</h1>
       <ProductList products={products.data} />
     </div>
   );
